45b9197abdea4bcd39d1e3bf05177371e99fde48
johcam <jocherrerapi@unal.edu.co>
2015-09-18 23:17:11 -0500

    Excepción no numérica

--- Taller_1/Integers.java
+++ Taller_1/Integers.java
@@ -4,7 +4,6 @@
  * and open the template in the editor.
  */
 
-
 import java.util.Scanner;
 /**
  *
@@ -12,18 +11,63 @@ import java.util.Scanner;
  */
 public class Integers {
     public static void main(String[] args) {
-        int num1;
-        int num2;
-        Scanner rd = new Scanner (System.in);
+        int num1=0;
+        int num2=0;
+        int larg;
         String num3;
         String num4;
-        System.out.println("Escriba el primer numero: ");
-        while (rd.hasNext()){
-            num3=rd.nextLine();
-            System.out.println("Escriba el segundo numero: ");
-            num4=rd.nextLine();
-            num1=Integer.parseInt(num3);
-            num2=Integer.parseInt(num4);
+        char ver;
+        boolean check=true;
+        Scanner rd = new Scanner (System.in);
+//        while (rd.hasNext()){
+            while (check){
+                System.out.println("Escriba el primer numero: ");
+                num3=rd.nextLine();
+                larg=num3.length();
+                int D=1;
+                int E=0;
+                for (int C=0; larg!=C;C++) {
+                    ver=num3.substring(D-1,D).charAt(0);
+                    if(Character.isDigit(ver)==false){                
+                        E=1;
+//                    }else{
+//                        E=1;
+                    }
+                    D++;
+                }
+                if (E==1){
+                    System.out.println("Su numero es invalido, intente nuevamente");                    
+                }else{
+                    check=false;
+                    num1=Integer.parseInt(num3);
+                }
+            }
+            System.out.println("Num1: "+num1);
+            check=true;
+            //ver=0;
+            while (check){
+                System.out.println("Escriba el segundo numero: ");
+                num4=rd.nextLine();
+                larg=num4.length();
+                int D=1;
+                int E=0;
+                for (int C=0; larg!=C;C++) {
+                    ver=num4.substring(D-1,D).charAt(0);
+                    if(Character.isDigit(ver)==false){                
+                        E=1;
+//                    }else{
+//                        E=1;
+                    }
+                    D++;
+                }
+                if (E==1){
+                    System.out.println("Su numero es invalido, intente nuevamente");                    
+                }else{
+                    check=false;            
+                    num2=Integer.parseInt(num4);
+                }
+            }
+            System.out.println("Num2: "+num2);
             if ((num1%2)==0){
                 System.out.println("El numero "+num1+" es par");
             }else if ((num1%2)!=0){
@@ -51,7 +95,7 @@ public class Integers {
             }else if (num1==num2){
                 System.out.println("Los dos números ingresados son iguales");
             }
-            break;
-        }
+//            break;
+//        }
     }
 }
