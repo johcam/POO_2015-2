17f9591b6b4786c47fdcb751557fb5c098fcdb20
johcam <jocherrerapi@unal.edu.co>
2015-10-20 10:08:55 -0500

    Some plane characteristics added

--- Parcial_1/carga.java
+++ Parcial_1/carga.java
@@ -1,6 +1,13 @@
 public class carga extends Aviones{
+    int capacidad;
+    int carga;
     
-        public int pasajeros(int n){
-        return 5;
+    public carga(int capacidad, int carga){
+        this.capacidad=capacidad;
+        this.carga=carga;
+    }
+    public int cargado(int carga){
+        this.capacidad=this.capacidad-carga;        
+        return this.capacidad-this.carga;
     }
 }
