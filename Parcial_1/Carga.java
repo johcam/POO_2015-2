3a40f66ae4d961c27390d0700677efe0f78b0bef
johcam <jocherrerapi@unal.edu.co>
2015-10-24 12:00:30 -0500

    Update and rename carga.java to Carga.java

--- Parcial_1/Carga.java
+++ Parcial_1/Carga.java
@@ -1,13 +1,13 @@
-public class carga extends Aviones{
+public class Carga extends Aviones{
     int capacidad;
     int carga;
     
-    public carga(int capacidad, int carga){
+    public Carga(int capacidad, int carga){
         this.capacidad=capacidad;
         this.carga=carga;
     }
     public int cargado(int carga){
-        this.capacidad=this.capacidad-carga;        
+        this.capacidad=this.capacidad-carga;
         return this.capacidad-this.carga;
     }
 }
